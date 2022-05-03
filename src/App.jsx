b43f28e7966c497a5647e4cfc37b1f9d029878aa
Philippe Ngom <philngom@gmail.com>
2022-05-03 11:27:58 -0700

    used useEffect to make fetch call to api. Wrapped app with router hook in index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { useEffect, useState } from 'react';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+
+  const [characters, setCharacters] = useState();
+
+  useEffect(async () => {
+    const res = await fetch('https://rickandmortyapi.com/api/character');
+    const data = await res.json();
+
+    setCharacters(data.results);
+
+  }, [])
+
+  return (
+
+  <h1>Hello World</h1>
+
+  );
 }
