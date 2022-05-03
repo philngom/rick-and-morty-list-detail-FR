cc8f3c3aaa861bfa46b2fd8bdf659b758ac15d74
Philippe Ngom <philngom@gmail.com>
2022-05-03 13:00:06 -0700

    add switch and route component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from 'react';
 import Characters from './views/Characters';
+import Characters from './component/Characters';
+import { Switch, Route } from 'react-router-dom';
 
 export default function App() {
 
   return (
-
-    <Characters />
+    <Switch>
+      <Route path="/characters">
+        <Characters />
+      </Route>
+      <Route path="/character/:id">
+        <Character />
+      </Route>
+    </Switch>
 
   );
 }
