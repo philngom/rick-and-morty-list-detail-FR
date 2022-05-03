8a15c7f241e1a7d5eba3590c358ae4d7714d8de4
Philippe Ngom <philngom@gmail.com>
2022-05-03 11:50:43 -0700

    added loading feature

--- src/views/Characters.jsx
+++ src/views/Characters.jsx
@@ -4,24 +4,35 @@ import { useEffect, useState } from 'react';
 export default function Characters() {
 
   const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(async () => {
     const res = await fetch('https://rickandmortyapi.com/api/character');
     const data = await res.json();
 
     setCharacters(data.results);
+    setIsLoading(false);
 
   }, [])
 
   return (
     <>
+    {
+      isLoading ? <p>Loading...</p>
+      : (
       <ul>
         {
           characters.map((character) => {
-            return <li>{character.name}</li>
+            return (
+              <article key={character.id}>
+                <li>{character.name}</li>
+              </article>
+            )
           })
         }
       </ul>
+      )
+    }
     </>
   )
 }
